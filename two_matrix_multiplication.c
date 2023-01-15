42a59dd24761aa6d21295295f2b9002a86561bc7
MadhavMadan336 <122387627+MadhavMadan336@users.noreply.github.com>
2023-01-15 12:18:56 +0530

    Two Matrix Multiplication {2*2}
    Two Matrix Multiplication {2*2}

---
+++ two_matrix_multiplication.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+int a[2][2],b[2][2],c[2][2],i,j,k;
+ printf("enter values for first matrix");
+	for(i=0;i<2;i++)
+    for(j=0;j<2;j++)
+	{
+		printf("\n enter values for 2D array=>>>>");
+		scanf("%d",&a[i][j]);
+	}
+printf("enter values for second matrix");
+    for(i=0;i<2;i++)
+    for(j=0;j<2;j++)
+	{
+		printf("\n enter values for 2D array=>>>>");
+		scanf("%d",&b[i][j]);
+	}
+printf("\n first matrix is ");  
+   for(i=0;i<2;i++)
+   {
+    printf("\n");
+    for(j=0;j<2;j++)
+    printf("%d\t",a[i][j]);
+   }  
+printf("\n second matrix is ");
+   for(i=0;i<2;i++)
+   {
+    printf("\n");
+    for(j=0;j<2;j++)
+    printf("%d\t",b[i][j]);
+   }  
+for(i=0;i<2;i++)
+for(j=0;j<2;j++)
+for(k=0;k<2;k++)
+c[i][j]=a[i][j]*b[i][j];
+printf("\n multiplacation of matrix ");
+  for(i=0;i<2;i++)
+   {
+    printf("\n");
+    for(j=0;j<2;j++)
+    printf("%d\t",c[i][j]);
+   }  
+return 0;
+}
