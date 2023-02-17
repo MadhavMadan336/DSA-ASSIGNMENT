cd0b95a6c3c625ea91c8df9afa2d80e5cfe17c7d
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-02-17 22:14:40 +0530

    Create creat_linklist.c

---
+++ Single LinkList/creat_linklist.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+int main()
+{
+    struct Node *head=NULL;
+    head=(struct Node *)malloc(sizeof(struct Node));
+    head->data=5;
+    head->next=NULL;
+    printf("%d",head->data);
+    return 0;
+}
